c338fa653d0365548774beb7fa84495063ff9469
JERVINFRANCISCO <FRAN0436@ALGONQUINLIVE.COM>
2021-12-21 13:56:36 -0500

    ds

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCkB6MMwDWAhFCMbIkjBHod6jJpnyemrzk",
+    authDomain: "pwa-app-71f0a.firebaseapp.com",
+    projectId: "pwa-app-71f0a",
+    storageBucket: "pwa-app-71f0a.appspot.com",
+    messagingSenderId: "6983471809",
+    appId: "1:6983471809:web:9b5f702ce27ef8e3a8fa40"
+  };
+
