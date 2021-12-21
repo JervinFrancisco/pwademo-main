92943a302ed72dd0bdad9e7d681024e0964c1c07
JERVINFRANCISCO <FRAN0436@ALGONQUINLIVE.COM>
2021-12-21 13:43:43 -0500

    ds

--- src/index.js
+++ src/index.js
@@ -8,15 +8,6 @@ navigator.serviceWorker.register("sw.js").then(registration =>{
 })
 }
 
-if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("./firebase-messaging-sw.js").then(registration =>{
-        console.log("SW Registered!");
-        console.log(registration)
-    }).catch(error =>{
-        console.log("SW Registered Failed!sdasdsd");
-        console.log(error)
-    })
-    }
     
 
 let cardElement = document.querySelector(".card");
