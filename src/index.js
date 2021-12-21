67daeba30730a4701339a7095e7bf8d04a0a1db0
JERVINFRANCISCO <FRAN0436@ALGONQUINLIVE.COM>
2021-12-21 12:52:01 -0500

    ch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 if("serviceWorker" in navigator){
-navigator.serviceWorker.register("sw.js", "firebase-messaging-sw.js").then(registration =>{
+navigator.serviceWorker.register("sw.js").then(registration =>{
     console.log("SW Registered!");
     console.log(registration)
 }).catch(error =>{
