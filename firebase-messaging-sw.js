1d03dda0fe6aceb86bbb374b1f8db618e7590ee2
Jervin Francisco <32100596+JervinFrancisco@users.noreply.github.com>
2021-12-21 12:20:40 -0500

    fr

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-messaging.js";
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCkB6MMwDWAhFCMbIkjBHod6jJpnyemrzk",
+    authDomain: "pwa-app-71f0a.firebaseapp.com",
+    projectId: "pwa-app-71f0a",
+    storageBucket: "pwa-app-71f0a.appspot.com",
+    messagingSenderId: "6983471809",
+    appId: "1:6983471809:web:9b5f702ce27ef8e3a8fa40"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const messaging = getMessaging();
+
+  console.log("hey")
