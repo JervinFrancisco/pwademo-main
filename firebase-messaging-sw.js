f53a217ff205047ab0dd55d23f772b20421bf34e
JERVINFRANCISCO <FRAN0436@ALGONQUINLIVE.COM>
2021-12-21 12:41:52 -0500

    ch

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
 import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-messaging.js";
 
@@ -16,25 +15,3 @@ const firebaseConfig = {
   const messaging = getMessaging();
 
   console.log("hey")
-=======
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-messaging.js";
-
-
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCkB6MMwDWAhFCMbIkjBHod6jJpnyemrzk",
-    authDomain: "pwa-app-71f0a.firebaseapp.com",
-    projectId: "pwa-app-71f0a",
-    storageBucket: "pwa-app-71f0a.appspot.com",
-    messagingSenderId: "6983471809",
-    appId: "1:6983471809:web:9b5f702ce27ef8e3a8fa40"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-
-  const messaging = getMessaging();
-
-
->>>>>>> e3f1a247cfd4ba3301942d2864c3b141bdbe8e86
