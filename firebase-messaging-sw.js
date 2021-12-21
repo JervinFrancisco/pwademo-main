8788a8f79d360cdcfc7470e8fad17e9ba7a52ce6
JERVINFRANCISCO <FRAN0436@ALGONQUINLIVE.COM>
2021-12-21 14:01:31 -0500

    ds

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,10 +1,9 @@
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCkB6MMwDWAhFCMbIkjBHod6jJpnyemrzk",
-    authDomain: "pwa-app-71f0a.firebaseapp.com",
-    projectId: "pwa-app-71f0a",
-    storageBucket: "pwa-app-71f0a.appspot.com",
-    messagingSenderId: "6983471809",
-    appId: "1:6983471809:web:9b5f702ce27ef8e3a8fa40"
-  };
-
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../firebase-messaging-sw.js')
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    }).catch(function(err) {
+      console.log('Service worker registration failed, error:', err);
+    });
+  }
